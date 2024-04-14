cc29f79655e4859cec78024cdd6d73dbb74b396e
engchina <engchina>
2024-04-14 20:27:34 +0800

    update

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 import os
+from typing import List, Union
 
 from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from langchain.prompts import ChatPromptTemplate
 from langchain_anthropic import ChatAnthropic
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.prompts import MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from langserve import add_routes
+from openai import BaseModel
+from pydantic import Field
 
 _ = load_dotenv(find_dotenv())
 
@@ -38,13 +43,41 @@ add_routes(
     path="/anthropic",
 )
 
-model = ChatAnthropic(model_name="claude-3-opus-20240229")
+model = ChatAnthropic(model_name="claude-3-sonnet-20240229")
 prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
 add_routes(
     app,
     prompt | model,
     path="/joke",
 )
+
+# # Declare a chain
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", "You are a helpful, professional assistant named Cob."),
+#         MessagesPlaceholder(variable_name="messages"),
+#     ]
+# )
+#
+# chain = prompt | ChatAnthropic(model="claude-2")
+#
+#
+# class InputChat(BaseModel):
+#     """Input for the chat endpoint."""
+#
+#     messages: List[Union[HumanMessage, AIMessage, SystemMessage]] = Field(
+#         ...,
+#         description="The chat messages representing the current conversation.",
+#     )
+#
+#
+# add_routes(
+#     app,
+#     chain.with_types(input_type=InputChat),
+#     enable_feedback_endpoint=True,
+#     enable_public_trace_link_endpoint=True,
+#     playground_type="chat",
+# )
 
 if __name__ == "__main__":
     import uvicorn
